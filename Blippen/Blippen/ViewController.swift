07a80b0dd35392cc63dce9a7c8032b036654a53e
Olle Vidner <olle@vidner.se>
2017-02-20 02:31:48 +0100

    Added auth support and made some minor code and UI fixes

--- Blippen/Blippen/ViewController.swift
+++ Blippen/Blippen/ViewController.swift
@@ -11,25 +11,67 @@ import WebKit
 
 class ViewController: UIViewController, WKNavigationDelegate, UITextFieldDelegate {
     
-    var webView: WKWebView?
+    var webView = WKWebView()
+    var modalOpen = false
     
     // Input field for RFID number
     @IBOutlet weak var inputField: UITextField!
     
+    func webView(_ webView: WKWebView, didReceive challenge: URLAuthenticationChallenge, completionHandler: @escaping (URLSession.AuthChallengeDisposition, URLCredential?) -> Void) {
+        
+        if challenge.protectionSpace.authenticationMethod == NSURLAuthenticationMethodHTTPBasic {
+            let storedCredentials = URLCredentialStorage.shared.defaultCredential(for: challenge.protectionSpace)
+            
+            if storedCredentials !== nil && challenge.previousFailureCount == 0 {
+                // Try stored credentials if they have not failed previously
+                completionHandler(.useCredential, storedCredentials)
+            } else {
+                // If no or bad credentials, open a modal dialog and ask for credentials
+                
+                let alertController = UIAlertController(title: "Log in", message: "This app is for internal use only.", preferredStyle: .alert)
+                alertController.addTextField(configurationHandler: { textField in
+                    textField.placeholder = "user name"
+                })
+                alertController.addTextField(configurationHandler: { textField in
+                    textField.placeholder = "password"
+                    textField.isSecureTextEntry = true
+                })
+                alertController.addAction(UIAlertAction(title: "Log in", style: .default) { action in
+                    // Store credentials in the keychain and send them to the server
+                    let enteredCredentials = URLCredential(
+                        user: (alertController.textFields?[0].text)!,
+                        password: (alertController.textFields?[1].text)!,
+                        persistence: .permanent)
+                    URLCredentialStorage.shared.setDefaultCredential(enteredCredentials, for: challenge.protectionSpace)
+                    completionHandler(.useCredential, enteredCredentials)
+                    self.inputField.becomeFirstResponder()
+                })
+                
+                modalOpen = true
+                inputField.resignFirstResponder()
+                present(alertController, animated: true, completion: {
+                    self.modalOpen = false
+                    self.inputField.becomeFirstResponder()
+                })
+            }
+        } else {
+            completionHandler(.performDefaultHandling, nil)
+        }
+        
+    }
+    
     func textFieldShouldEndEditing(_ textField: UITextField) -> Bool {
-        // Never release focus from the text field
-        return false
+        // Never release focus from the text field unless the web view is loading ('loading' includes waiting for the log in modal to be dismissed)
+        return webView.isLoading
     }
     
     func textFieldShouldReturn(_ textField: UITextField) -> Bool {
         // Run when enter is pressed in the text field.
-        let rfidNumber: String = textField.text!
         
         // So, why do this instead of just using the field in the web view? It turns out that the card reader spits out its data
         // to fast for the web view, resulting in garbled data. So we use a native text field (which behaves nicely) instead.
-        //
-        webView?.evaluateJavaScript("document.getElementById('rfid').value='\(rfidNumber)';", completionHandler: nil)
-        webView?.evaluateJavaScript("document.getElementById('form').dispatchEvent(new Event('submit'));", completionHandler: nil)
+        webView.evaluateJavaScript("document.getElementById('rfid').value='\(textField.text!)';", completionHandler: nil)
+        webView.evaluateJavaScript("document.getElementById('form').dispatchEvent(new Event('submit'));", completionHandler: nil)
         
         // Resets the text field for the next entry.
         textField.text = ""
@@ -40,9 +82,10 @@ class ViewController: UIViewController, WKNavigationDelegate, UITextFieldDelegat
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        webView = WKWebView(frame: view.bounds)
-        webView?.navigationDelegate = self
-        view.addSubview(webView!)
+        webView.frame = view.bounds
+        
+        webView.navigationDelegate = self
+        view.addSubview(webView)
         
         inputField.delegate = self
         
@@ -50,15 +93,9 @@ class ViewController: UIViewController, WKNavigationDelegate, UITextFieldDelegat
         inputField.becomeFirstResponder()
         
         // Load the url into the webview
-        let blippURL = URL(string: "https://blipper:Blipper123@blipp.baljan.org")!
-        webView?.load(URLRequest(url: blippURL))
+        let blippURL = URL(string: "https://blipp.baljan.org/")
+        
+        webView.load(URLRequest(url: blippURL!))
     }
-    
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
-
 }
 
